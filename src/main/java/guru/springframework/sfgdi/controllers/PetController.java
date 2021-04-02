2600639496c6cbac593eef5c51feed7d0f4cac83
absawant25 <abhisawant25@gmail.com>
2021-04-02 17:51:21 +0530

    Assigment Completed.

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.sfgdi.services.PetService;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
+	@Autowired
     private final PetService petService;
+	
+	public PetController(@Qualifier("PetService") PetService petService) {
+		this.petService = petService;
+	}
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
